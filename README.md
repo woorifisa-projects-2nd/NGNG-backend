08d4542d9fac50b4409e1c8823d2c68d7c6784dd
sws6641 <sws6641@naver.com>
2024-04-09 16:42:46 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# NGNG-backend
---
+++ src/test/java/com/ngng/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngng.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

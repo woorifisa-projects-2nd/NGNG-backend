3c5549664537e21b2058f61a025d2997bdfe7ce0
juchan <rlawncks125@naver.com>
2024-04-24 09:39:59 +0900

    Fix : 상품 생성시 refreshedAt 생성 날짜로 설정

--- src/main/java/com/ngng/api/product/entity/Product.java
+++ src/main/java/com/ngng/api/product/entity/Product.java
@@ -16,6 +16,7 @@ import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
 
 @Setter
@@ -108,6 +109,7 @@ public class Product {
         this.freeShipping = request.getFreeShipping();
         this.user = User.builder().userId(request.getUserId()).build();
         this.status = Status.builder().statusId(request.getStatusId()).build();
+        this.refreshedAt = new Timestamp(new Date().getTime());
         this.category = Category.builder().categoryId(request.getCategoryId()).build();
         this.tags = request.getTags().stream().
                 map(tag -> Tag.builder()

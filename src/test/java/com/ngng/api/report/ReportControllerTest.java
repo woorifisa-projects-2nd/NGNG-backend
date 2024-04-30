ba32cd6383efb154b84650f132ddd31be18736a3
chomyungha <chomyungha@gmail.com>
2024-04-30 11:06:44 +0900

    Feat. report controller test

--- src/test/java/com/ngng/api/report/ReportControllerTest.java
+++ src/test/java/com/ngng/api/report/ReportControllerTest.java
@@ -2,7 +2,7 @@ package com.ngng.api.report;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ngng.api.penalty.dto.CreatePenaltyRequestDTO;
+import com.ngng.api.report.dto.CreateReportRequestDTO;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,10 +17,12 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+@AutoConfigureMockMvc(addFilters = false)
+@SpringBootTest
+@Transactional
+@WithUserDetails(value = "jin@gmail.com" , setupBefore = TestExecutionEvent.TEST_EXECUTION)
 public class ReportControllerTest {
 
     @Autowired
@@ -31,4 +33,73 @@ public class ReportControllerTest {
     PlatformTransactionManager transactionManager;
 
     TransactionStatus status;
+    @BeforeEach
+    void beforeEach() {
+        // 트랜잭션 시작
+        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+    }
+
+    @AfterEach
+    void afterEach() {
+        // 트랜잭션 롤백
+        transactionManager.rollback(status);
+    }
+
+    // readAll test
+    @Test
+    void readAll() throws Exception {
+        mvc.perform(get("/admin/reports")).andExpect(status().isOk());
+    }
+
+    // read test
+    @Test
+    void read() throws Exception {
+        Long reportId = 33L;
+        mvc.perform(get("/admin/reports/"+reportId)).andExpect(status().isOk());
+    }
+
+    // update test
+    @Test
+    void update() throws  Exception {
+        Long reportId = 33L;
+        mvc.perform(patch("/admin/reports/"+reportId))
+                .andExpect(status().isOk());
+    }
+
+    // delete test
+    @Test
+    void deleteTest() throws  Exception {
+        Long reportId = 33L;
+        mvc.perform(delete("/admin/reports/" + reportId))
+                .andExpect(status().isOk());
+    }
+
+    // create test
+    @Test
+    void create() throws Exception {
+        String reportContents = "신고내용";
+        Long reportTypeId = 3L;
+        Long reporterId = 2L;
+        Long userId = 1L;
+        Boolean isReport = false;
+        Long productId = 7L;
+        Long privateChatId = 1L;
+        CreateReportRequestDTO requestDto = new CreateReportRequestDTO(
+                reportContents,
+                reportTypeId,
+                reporterId,
+                userId,
+                isReport,
+                productId,
+                privateChatId
+        );
+        String jsonStr = objectMapper.writeValueAsString(requestDto);
+        mvc.perform(post("/reports")
+                .content(jsonStr)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isCreated());
+    }
+
+
+
 }
